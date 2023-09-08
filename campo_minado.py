904c8f6f4a273e8b4a626eebf29dd4fa6d9938c6
Ana Clara <anaclaraguimaraesremotto@gmail.com>
2023-09-08 00:01:09 -0300

    coloca numeros no tabuleiro

--- campo_minado.py
+++ campo_minado.py
@@ -15,4 +15,18 @@ def posicionar_bombas(matriz, bombas):
         if matriz[row][col] != -1:
             matriz[row][col] = -1
             bombas_colocadas += 1
-            
+
+def colocar_numeros(matriz):
+    rows, cols = len(matriz), len(matriz[0])
+
+    for row in range(rows):
+        for col in range(cols):
+            if matriz[row][col] != -1:
+                bombas_vizinhas = 0
+
+                for i in range(-1, 2):
+                    for j in range(-1, 2):
+                        if 0 <= row + i < rows and 0 <= col + j and matriz[row + i][col + j] == -1:
+                            bombas_vizinhas += 1
+                
+                matriz[row][col] = bombas_vizinhas
